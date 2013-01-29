03a76db4c2604d119ff01e9a3109ba4791a5417a
Kris Olszewski <kris.olszewski@gmail.com>
2013-01-29 09:59:06 -0500

    Update active-menu.js

--- active-menu.js
+++ active-menu.js
@@ -6,8 +6,8 @@
  * Repo: https://github.com/KrisOlszewski/ActiveMenu/
  */
 (function($, window, document, undefined) {
-	
-	// Utility function for older browsers
+
+	// Utility function
 	if (typeof Object.create !== 'function') {
 		Object.create = function(obj) {
 			function F() {}
@@ -26,11 +26,12 @@
 			self.pathname = window.location.pathname;
 			self.pathname = self.split(self.pathname);
 			self.pathname = self.cleanup(self.pathname);
-			self.pathname = self.drop(self.pathname);
+			self.pathname = self.dropExtension(self.pathname);
+			self.pathname = self.lowerCase(self.pathname);
 
 			self.link = $(container).find('a');
 			self.link.each(function() {
-				self.display(this);
+				self.setState(this);
 			});
 
 		},
@@ -42,23 +43,29 @@
 				return value !== '';
 			});
 		},
-		drop: function(array) {
+		dropExtension: function(array) {
 			return $.each(array, function(key) {
 				return array[key] = array[key].substr(0, array[key].lastIndexOf('.')) || array[key];
 			});
 		},
-		display: function(link) {
+		lowerCase: function(array) {
+			return $.each(array, function(key) {
+				return array[key] = array[key].toLowerCase();
+			});	
+		},
+		setState: function(link) {
 
 			var self = this,
 				$link = $(link),
 				href = $link.attr('href'),
 				elem = (self.options.parent) ? $link.parent() : $link;
-			
-			if (href === undefined) {return;}
+
+			if (href === undefined) { return; }
 			
 			href = self.split(href);
 			href = self.cleanup(href);
-			href = self.drop(href);
+			href = self.dropExtension(href);
+			href = self.lowerCase(href);
 
 			var withOutIndex = (href.length === 0 && self.pathname.length === 0),
 				withIndex = (href.length === 0 && self.pathname.length === 1 && self.pathname[0] === 'index');
